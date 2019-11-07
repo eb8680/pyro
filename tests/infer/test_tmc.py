ad2073c76a067867f2ab0e30aa314ac097a2e05d
Eli <eli.bingham@uber.com>
2019-11-06 16:15:07 -0800

    add normal chain gradient tests for factorized and nonfactorized guides

--- tests/infer/test_tmc.py
+++ tests/infer/test_tmc.py
@@ -74,7 +74,8 @@ def test_tmc_categoricals(depth, max_plate_nesting, num_samples):
 @pytest.mark.parametrize("num_samples", [500])
 @pytest.mark.parametrize("max_plate_nesting", [0])
 @pytest.mark.parametrize("reparameterized", [True, False])
-def test_tmc_normals(depth, num_samples, max_plate_nesting, reparameterized):
+def test_tmc_normals_iwae(depth, num_samples, max_plate_nesting, reparameterized):
+    # compare reparameterized and nonreparameterized gradient estimates
     pyro.clear_param_store()
 
     q1 = pyro.param("q1", torch.tensor(0.5, requires_grad=True))
@@ -119,6 +120,7 @@ def test_tmc_normals(depth, num_samples, max_plate_nesting, reparameterized):
 @pytest.mark.parametrize("num_samples,expand", [(500, True), (500, False)])
 @pytest.mark.parametrize("max_plate_nesting", [0, 1])
 def test_tmc_normals_gradient(depth, num_samples, max_plate_nesting, expand):
+    # compare reparameterized and nonreparameterized gradient estimates
     pyro.clear_param_store()
 
     q1 = pyro.param("q1", torch.tensor(0.5, requires_grad=True))
@@ -137,6 +139,60 @@ def test_tmc_normals_gradient(depth, num_samples, max_plate_nesting, expand):
     expected_grads = grad(expected_loss, (q1,))
 
     actual_loss = tmc.differentiable_loss(tmc_model, lambda x: None, False)
+    actual_grads = grad(actual_loss, (q1,))
+
+    # TODO increase this precision, suspiciously weak
+    assert_equal(actual_loss, expected_loss, prec=0.05, msg="".join([
+        "\nexpected loss = {}".format(expected_loss),
+        "\n  actual loss = {}".format(actual_loss),
+    ]))
+
+    # TODO increase this precision, suspiciously weak
+    for actual_grad, expected_grad in zip(actual_grads, expected_grads):
+        assert_equal(actual_grad, expected_grad, prec=0.05, msg="".join([
+            "\nexpected grad = {}".format(expected_grad.detach().cpu().numpy()),
+            "\n  actual grad = {}".format(actual_grad.detach().cpu().numpy()),
+        ]))
+
+
+@pytest.mark.parametrize("depth", [1, 2, 3])
+@pytest.mark.parametrize("num_samples,expand", [(500, True), (500, False)])
+@pytest.mark.parametrize("max_plate_nesting", [0, 1])
+@pytest.mark.parametrize("factorized", [True, False])
+def test_tmc_normals_gradient_guide(depth, num_samples, max_plate_nesting, expand, factorized):
+    # compare reparameterized and nonreparameterized gradient estimates
+    pyro.clear_param_store()
+
+    q1 = pyro.param("q1", torch.tensor(0.5, requires_grad=True))
+
+    def model(reparameterized):
+        Normal = dist.Normal if reparameterized else fakes.NonreparameterizedNormal
+        x = pyro.sample("x0", Normal(0., 1.))
+        for i in range(1, depth):
+            x = pyro.sample("x{}".format(i), Normal(x, 1.))
+        pyro.sample("y", Normal(x, 1.), obs=torch.tensor(float(1)))
+
+    def factorized_guide(reparameterized):
+        Normal = dist.Normal if reparameterized else fakes.NonreparameterizedNormal
+        pyro.sample("x0", Normal(pyro.param("q1"), 1. / depth))
+        for i in range(1, depth):
+            pyro.sample("x{}".format(i), Normal(0., float(i+1 / depth)))
+
+    def nonfactorized_guide(reparameterized):
+        Normal = dist.Normal if reparameterized else fakes.NonreparameterizedNormal
+        x = pyro.sample("x0", Normal(pyro.param("q1"), 1. / depth))
+        for i in range(1, depth):
+            x = pyro.sample("x{}".format(i), Normal(x, 1. / depth))
+
+    tmc = TensorMonteCarlo(max_plate_nesting=max_plate_nesting)
+    tmc_guide = config_enumerate(
+        factorized_guide if factorized else nonfactorized_guide,
+        default="parallel", expand=expand, num_samples=num_samples)
+
+    expected_loss = tmc.differentiable_loss(model, tmc_guide, True)
+    expected_grads = grad(expected_loss, (q1,))
+
+    actual_loss = tmc.differentiable_loss(model, tmc_guide, False)
     actual_grads = grad(actual_loss, (q1,))
 
     # TODO increase this precision, suspiciously weak
